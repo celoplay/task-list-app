e51e9c9af0e77ed6fcbe780263e19a74e5a6d5eb
celoplay <celoaizaga@gmail.com>
2025-07-21 23:48:26 +0200

    datepipe

--- src/app/components/task-list/components/task/task.ts
+++ src/app/components/task-list/components/task/task.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITask } from '../../../../models/task.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.html',
-  styleUrl: './task.scss'
+  styleUrl: './task.scss',
 })
 export class Task {
-
+  @Input({ required: true }) task!: ITask;
+  @Input() index: number = 0;
 }
--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ITask } from '../../models/task.model';
+import { Task } from "./components/task/task";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [Task], 
   templateUrl: './task-list.html',
   styleUrl: './task-list.scss'
 })
